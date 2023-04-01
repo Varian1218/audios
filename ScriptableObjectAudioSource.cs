5f67a115a14c8c0353876f98bfd774d7371e1cbf
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-01 19:54:33 +0700

    1.0.0

---
+++ ScriptableObjectAudioSource.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Audios
+{
+    [CreateAssetMenu(menuName = "Audios/Audio Source", fileName = "Audio Source", order = 1)]
+    public class ScriptableObjectAudioSource : ScriptableObject
+    {
+        private AudioSource _impl;
+
+        public AudioSource Impl
+        {
+            set => _impl = value;
+        }
+
+        public void Play(AudioClip clip)
+        {
+            _impl.clip = clip;
+            _impl.Play();
+        }
+    }
+}
