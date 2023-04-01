f6ead0b2be004c8061fb9f097f8a90a9b6d96c5b
DESKTOP-UJRR6FS\Admin <tuduchoang@gmail.com>
2023-04-01 20:30:46 +0700

    1.0.1

---
+++ LoadAudioAction.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Audios
+{
+    [CreateAssetMenu(
+        menuName = "Audios/Load Audio Action",
+        fileName = "Load Audio Action",
+        order = 1
+    )]
+    public class LoadAudioAction : ScriptableObject
+    {
+        [SerializeField] private AudioSourceBuilder builder;
+        [SerializeField] private ScriptableObjectAudioSource source;
+
+        public void Invoke()
+        {
+            source.Impl = builder.Build();
+        }
+    }
+}
